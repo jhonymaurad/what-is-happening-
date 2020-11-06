aaf356ccb8227f84b49d01f99023adca82dd8456
jhony maurad <jhonymaurad@gmail.com>
2020-11-06 00:32:01 -0500

    Initial project setup, installed standardx for linting, created Header component

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='header'>
+
+      <a className='header__logo'>
+        Streamer
+      </a>
+
+      <div className='header__right'>
+
+        All Streams
+
+      </div>
+    </div>
+  )
+}
+
+export default Header;
